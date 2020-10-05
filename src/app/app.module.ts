95672e7461ad3b21224d5f288a66b1362423c0a3
Stephen Kline <Stephenkline09@gmail.com>
2020-10-05 15:29:39 -0500

    Trouble with imports on app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule, MatIconModule, MatButtonModule, MatCardModule, MatGridListModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/character.ts
+++ src/app/character.ts
@@ -5,6 +5,5 @@
 //import { SIGPIPE } from "constants";
 
   const Character = [
-    { id: 1, name: 'Spider Man', image: 'assets/SpiderMan.jpg', identity: "Peter Parker", powers: 'Genius intellect, proficient scientist and inventor, superhuman strength, speed, durability, agility, stamina, reflexes, coordination, balance and endurance. Precognitive spider-sense ability, cling to most solid surfaces and web ability. Also utilizes wrist web-shooters to shoot spiderweb material.'
+    { id: 1, name: 'Spider Man', image: 'assets/SpiderMan.jpg', identity: "Peter Parker", powers: 'Genius intellect, proficient scientist and inventor, superhuman strength, speed, durability, agility, stamina, reflexes, coordination, balance and endurance. Precognitive spider-sense ability, cling to most solid surfaces and web ability. Also utilizes wrist web-shooters to shoot spiderweb material.',
   }]
-
